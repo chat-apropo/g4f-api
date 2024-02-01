8ddf4b4f5bb6c2cb4965bc52e886e43274efcb7b
loudercake <jxzk@protonmail.com>
2024-02-01 16:35:20 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import g4f
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+@app.get("/3")
+def test(text: str):
+    try:
+        response = g4f.ChatCompletion.create(
+            model="gpt-3.5-turbo-16k-0613",
+            messages=[{"role": "user", "content": text}],
+            stream=False)
+    except:
+        raise HTTPException(status_code=404, detail="insert crappy error handling message here lol :)")
+    return {"message": response }

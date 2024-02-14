39ce8272fed92973db1c9e23ed68356da1ba5995
loudercake <jxzk@protonmail.com>
2024-02-14 15:10:20 -0300

    working api

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from pydantic import BaseModel
 
 class Message(BaseModel):
     role: Literal["user", "assistant"]
-    message: str
+    content: str
 
 
 class Completion(BaseModel):
@@ -20,10 +20,7 @@ app = FastAPI()
 
 @app.post("/")
 def test(completion: Completion):
-    try:
-        response = g4f.ChatCompletion.create(
-            model=completion.model, messages=[msg.model_dump() for msg in completion.messages], stream=False
+    response = g4f.ChatCompletion.create(
+        model=completion.model, messages=[msg.model_dump() for msg in completion.messages], stream=False
         )
-    except:
-        raise HTTPException(status_code=404, detail="error")
     return {"message": response}

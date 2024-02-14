0e298bceef10bc28eae7b208436bf6ceaf186f48
loudercake <jxzk@protonmail.com>
2024-02-14 13:29:42 -0300

    make post request for gpt completion

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import g4f
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+class Completion(BaseModel):
+    model: str
+    messages: List[dict]
 
 app = FastAPI(root_path="ai_api")
 
-@app.get("/3")
-def test(text: str):
+@app.post("/")
+def test(completion: Completion):
     try:
         response = g4f.ChatCompletion.create(
-            model="gpt-3.5-turbo-16k-0613",
-            messages=[{"role": "user", "content": text}],
+            model=completion.model,
+            messages=completion.messages,
             stream=False)
-    except:
-        raise HTTPException(status_code=404, detail="insert crappy error handling message here lol :)")
+    except
+        raise HTTPException(status_code=404, detail="error")
     return {"message": response }
+

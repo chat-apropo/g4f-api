2a7f7263efe658932795f4b92e47b2a8706ae9ed
Matheus Fillipe <matheusfillipeag@gmail.com>
2024-09-07 15:40:13 +0200

    Fixed tests?

--- backend/dependencies.py
+++ backend/dependencies.py
@@ -3,14 +3,18 @@ from typing import TypeVar
 import g4f
 from fastapi import Query
 from fastapi.openapi.models import Example
+from g4f.models import ModelUtils
+from g4f.Provider import RetryProvider
 from g4f.Provider.base_provider import ProviderModelMixin
-from g4f.models import ModelUtils, RetryProvider
-from g4f.providers import retry_provider
 from pydantic import BaseModel, Field
 
 from backend.models import CompletionModel, CompletionProvider, Message
 
-g4f_working_providers_map = {provider.__name__: provider for provider in g4f.Provider.__providers__ if provider.working}
+g4f_working_providers_map = {
+    provider.__name__: provider
+    for provider in g4f.Provider.__providers__
+    if provider.working
+}
 all_working_provider_names: list[str] = list(g4f_working_providers_map.keys())
 all_working_providers_map: dict[str, CompletionProvider] = {}
 
@@ -27,7 +31,9 @@ for model_name in all_model_names:
     else:
         best_providers.add(model.best_provider.__name__)
 
-    complation_model = CompletionModel(name=model.name, supported_provider_names=best_providers)
+    complation_model = CompletionModel(
+        name=model.name, supported_provider_names=best_providers
+    )
     all_models_map[model_name] = complation_model
 
     # Populate providers with recomended models
@@ -36,7 +42,9 @@ for model_name in all_model_names:
             all_working_providers_map[provider_name] = CompletionProvider(
                 name=provider_name,
                 supported_models=set(),
-                url=g4f_working_providers_map[provider_name].url if provider_name in g4f_working_providers_map else "",
+                url=g4f_working_providers_map[provider_name].url
+                if provider_name in g4f_working_providers_map
+                else "",
             )
         all_working_providers_map[provider_name].supported_models.add(model.name)
 
@@ -44,8 +52,13 @@ for model_name in all_model_names:
 for provider in g4f.Provider.__providers__:
     if not provider.working:
         continue
-    if isinstance(provider, ProviderModelMixin) and provider.__name__ in all_working_providers_map:
-        all_working_providers_map[provider.__name__].supported_models.update(provider.models)
+    if (
+        isinstance(provider, ProviderModelMixin)
+        and provider.__name__ in all_working_providers_map
+    ):
+        all_working_providers_map[provider.__name__].supported_models.update(
+            provider.models
+        )
 
 A = TypeVar("A")
 
@@ -65,17 +78,17 @@ def allowed_values_or_none(v: A | None, allowed: list[A]) -> A | None:
 class CompletionParams:
     def __init__(
         self,
-        model: str
-        | None = Query(
+        model: str | None = Query(
             None,
             description="LLM model to use for completion. Cannot be specified together with provider.",
             openapi_examples=generate_examples_from_values([None] + all_model_names),
         ),
-        provider: str
-        | None = Query(
+        provider: str | None = Query(
             None,
             description="Provider to use for completion. Cannot be specified together with model.",
-            openapi_examples=generate_examples_from_values([None] + all_working_provider_names),
+            openapi_examples=generate_examples_from_values(
+                [None] + all_working_provider_names
+            ),
         ),
     ):
         provider = provider or None
@@ -108,4 +121,6 @@ class UiCompletionRequest(BaseModel):
     message: str = Field(..., description="Current message from text input")
     model: str = Field(..., description="Model to use for completion")
     provider: str = Field(..., description="Provider to use for completion")
-    history: list[Message] = Field(default_factory=list, description="History of past messages")
+    history: list[Message] = Field(
+        default_factory=list, description="History of past messages"
+    )

c95f04a8dc58df370455fe8c8473dfccd7a2d5c6
Matheus Fillipe <matheusfillipeag@gmail.com>
2024-10-19 19:13:19 +0200

    reformat

--- backend/background.py
+++ backend/background.py
@@ -18,12 +18,16 @@ async def ai_respond(messages: list[dict], model: str, provider: ProviderType) -
     )
     choices = chat_completion.choices
     if len(choices) == 0:
-        raise CustomValidationError("No response from the provider", error={"messages": messages})
+        raise CustomValidationError(
+            "No response from the provider", error={"messages": messages}
+        )
 
     return choices[0].message.content
 
 
-async def test_provider(provider: ProviderType, queue: asyncio.Queue, semaphore: asyncio.Semaphore) -> bool:
+async def test_provider(
+    provider: ProviderType, queue: asyncio.Queue, semaphore: asyncio.Semaphore
+) -> bool:
     """Sends hi to a provider and check if there is response or error."""
     print(f"Testing provider {provider.__name__}")
     async with semaphore:
@@ -34,7 +38,11 @@ async def test_provider(provider: ProviderType, queue: asyncio.Queue, semaphore:
             elif hasattr(provider, "default_model"):
                 model = provider.default_model
             elif provider.__name__ in provider_and_models.all_working_providers_map:
-                model = list(provider_and_models.all_working_providers_map[provider.__name__].supported_models)[0]
+                model = list(
+                    provider_and_models.all_working_providers_map[
+                        provider.__name__
+                    ].supported_models
+                )[0]
             else:
                 model = "gpt-4"
             async with asyncio.timeout(5):
@@ -62,7 +70,9 @@ async def update_working_providers():
 
         async def producer():
             semaphore = asyncio.Semaphore(8)
-            await asyncio.gather(*[test_provider(provider, queue, semaphore) for provider in providers])
+            await asyncio.gather(
+                *[test_provider(provider, queue, semaphore) for provider in providers]
+            )
             await queue.join()
             await queue.put((None, None))
 
@@ -79,7 +89,12 @@ async def update_working_providers():
 
         await asyncio.gather(producer(), consumer())
 
-        print(f"Finished testing providers. Updating working providers to {len(now_working_providers)}")
+        print(
+            f"Finished testing providers. Updating working providers to {len(now_working_providers)}"
+        )
         provider_and_models.update_model_providers(
-            {provider_name: base_working_providers_map[provider_name] for provider_name in now_working_providers}
+            {
+                provider_name: base_working_providers_map[provider_name]
+                for provider_name in now_working_providers
+            }
         )
